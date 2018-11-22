45819373daf84b8e3965fe3e4bbb0618f1428573
evb0110 <7149553@gmail.com>
2018-11-22 15:06:33 +0300

    12 lectures

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,11 +1,76 @@
-'use strict';
+"use strict";
 
 console.log('App.js is running');
 
+var app = {
+  title: "Indecision app",
+  subtitle: "My first react app",
+  options: [
+    // "first option", "2nd option"
+  ]
+};
+
 var template = React.createElement(
-  'h1',
+  "div",
   null,
-  'Indecision App'
+  React.createElement(
+    "h1",
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    "p",
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    "p",
+    null,
+    app.options.length > 0 ? "Options present" : "No options"
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "first item"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "second item"
+    )
+  )
+);
+
+var user = {
+  name: 'Eugene',
+  age: 44,
+  location: 'ЦНЦ, Moscow'
+};
+
+var templateTwo = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "my name is ",
+    user.name
+  ),
+  React.createElement(
+    "p",
+    null,
+    "my age is ",
+    user.age
+  ),
+  React.createElement(
+    "p",
+    null,
+    "my location is ",
+    user.location
+  )
 );
 var appRoot = document.getElementById('app');
 
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,6 +1,40 @@
 console.log('App.js is running');
 
-var template = <h1>Indecision App</h1>;
+let app = {
+  title: "Indecision app",
+  subtitle: "My first react app",
+  options: [
+    // "first option", "2nd option"
+  ],
+};
+
+
+let template = ( 
+  <div>
+    <h1>{app.title}</h1>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? "Options present" : "No options"}</p>
+    <ol>
+      <li>first item</li>
+      <li>second item</li>
+    </ol>
+  </div>
+);
+
+
+let user = {
+  name: 'Vladimir',
+  age: 44,
+  location: 'Moscow',
+};
+
+var templateTwo = (
+    <div>
+      <h1>my name is {user.name}</h1>
+      <p>my age is {user.age}</p>
+      <p>my location is {user.location}</p>
+    </div>
+);
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template, appRoot); 
