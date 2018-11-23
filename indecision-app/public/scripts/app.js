9a24f50f3609ceaeb115c1f967a4aa0dc77a4839
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2018-11-23 15:14:32 +0300

    add es6-arrow-function-2.js to playground folder

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,15 +1,1 @@
 "use strict";
-
-// const square = function (x) {
-//   return x * x;
-// };
-
-// const squareArrow = x => x * x;
-
-// console.log(squareArrow(8));
-
-var getFirstName = function getFirstName(name) {
-  return name.split(' ')[0];
-};
-
-console.log(getFirstName("John Berkow"));
