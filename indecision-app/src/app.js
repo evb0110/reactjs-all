4085f3a874d606b0bf16fd1fb6de538181bcac63
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2018-11-26 19:05:51 +0300

    move forward through Andrew's lectures: scss, 3rd-party components etc

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,5 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+const Layout = (props) => {
+  return (
+    <div>
+      <p>header</p>
+      {props.children}
+      <p>footer</p>
+    </div>
+  )
+}
+
+const template = (
+  <div>
+    <h1>Page Title</h1>
+    <p>This is my page</p>
+  </div>
+);
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
