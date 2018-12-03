2d71c50798fb87da47c6b7fa52fb723599fcc47b
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2018-12-04 01:16:28 +0300

    added ErrorBoundary

--- robofriends/src/components/ErrorBoundary.js
+++ robofriends/src/components/ErrorBoundary.js
@@ -7,11 +7,17 @@ class ErrorBoundary extends Component {
       hasError: false
     }
   }
-}
 
-render() {
-  if (this.state.hasError) {
-    return <h1>Oooops. That is not good</h1>
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true })
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (<h1>Oooops. That is not good</h1>)
+    } else {
+      return this.props.children;
+    }
   }
 }
 
