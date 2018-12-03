063ab13e09911b39a7e10e35c6bcb984f9a28220
evb0110 <7149553@gmail.com>
2018-12-04 01:16:28 +0300

    added ErrorBoundary

--- robofriends/src/containers/Scroll.js
+++ robofriends/src/containers/Scroll.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Scroll = prop => {
+const Scroll = props => {
   return (
     <div style={{overflowY: 'scroll', border: '1px solid black', height: '800px'}}>
-      {prop.children}
+      {props.children}
     </div>
   );
 };
