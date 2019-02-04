8a3d890a11a68a300f04d594a6cba5debd24f050
evb0110 <7149553@gmail.com>
2019-02-05 01:04:15 +0300

    more redux

--- 01_mead_playing_redux/src/playground/redux-101.js
+++ 01_mead_playing_redux/src/playground/redux-101.js
@@ -1,49 +1,64 @@
-import { createStore } from "redux";
+import { createStore } from 'redux';
 
-const [INCREMENT, DECREMENT, RESET] = ["INCREMENT", "DECREMENT", "RESET"];
+const [INCREMENT, DECREMENT, RESET, SET] = ['INCREMENT', 'DECREMENT', 'RESET', 'SET'];
 
 const store = createStore((state = { count: 0 }, action) => {
+  const value = typeof action.value == 'number' ? action.value : 1;
   switch (action.type) {
     case INCREMENT:
       return {
-        count: state.count + (action.value || 1)
+        count: state.count + value
       };
-    case DECREMENT:
+      case DECREMENT:
       return {
-        count: state.count - (action.value || 1)
+        count: state.count - value
       };
     case RESET:
       return {
         count: 0
       };
+    case SET:
+      return {
+        count: value
+      }
     default:
       return state;
   }
 });
 
-console.log(store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
+
 store.dispatch({
   type: INCREMENT,
   value: 10
 });
 
-console.log(store.getState());
 store.dispatch({
   type: INCREMENT
 });
 
-console.log(store.getState());
 store.dispatch({
-  type: DECREMENT,
-  value: 4
+  type: 'abc'
 });
 
-console.log(store.getState());
+store.dispatch({
+  type: DECREMENT,
+  value: 14
+});
+
+store.dispatch({
+  type: DECREMENT
+});
+
 store.dispatch({
   type: RESET
 });
+
 store.dispatch({
-  type: "abc"
+  type: SET,
+  value: 101
 });
 
-console.log(store.getState());
+unsubscribe();
